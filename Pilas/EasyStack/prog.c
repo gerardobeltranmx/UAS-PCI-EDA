af0e7de2a67da68ffefab854bbf58a4784b9c651
gerardobeltranmx <gerardo@MacBook-Pro-de-Gerardo.local>
2021-03-09 12:48:44 -0700

    .

---
+++ Pilas/EasyStack/prog.c
@@ -0,0 +1,50 @@
+
+typedef int T;
+
+// Definición del la estructura de la Pila
+typedef struct{
+    int tam; //Tamaño de la pila
+    int tope;
+    T *elementos; //Datos de la pila con apuntador
+} Tpila;
+//Iniciar Pila
+void iniciarPila(Tpila *pila, int N){
+    pila->elementos= (T *) malloc(sizeof(T)*N);
+    pila->tam=N;
+    pila->tope=-1;
+}
+// Agregar un dato a la Pila
+void agregarPila(Tpila *pila, T dato){
+    pila->tope++;
+    pila->elementos[pila->tope]=dato;
+}
+
+// Extraer un dato de la Pila
+T extraerPila(Tpila *pila){
+    T dato;
+    dato = pila->elementos[pila->tope];
+    pila->tope--;
+    return dato;
+}
+//Obtener dato del tope sin extraerlo
+T obtenerElementoTopePila(Tpila pila){
+return pila.elementos[pila.tope];
+}
+
+//Determina si la Pila esta vacía
+int vaciaPila(Tpila pila){
+    eturn(pila.tope==-1)?1:0;
+}
+//Determina si la Pila esta llena
+int llenaPila(Tpilapila){
+    return(pila.tope==pila.tam-1)?1:0;
+}
+
+//Obtiene numero de elementos de la Pila
+int numElementosPila(Tpila pila){
+    return pila.tope+1;
+}
+//Obtiene un elemento de la Pila de alguna posición
+T obtenerElementoPila(Tpila pila, int pos){
+    return pila.elementos[pos];
+}
